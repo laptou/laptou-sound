feda8637e7a90223486768d1e82970d285ae7e5f
Ibiyemi Abiodun <ibiyemi@intulon.com>
2025-11-29 00:37:49 -0500

    add cf typegen

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
+import solid from "vite-plugin-solid";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -16,8 +16,8 @@ export default defineConfig({
 			projects: ["./tsconfig.json"],
 		}),
 		tailwindcss(),
-		tanstackStart(),
-		solidPlugin({ ssr: true }),
 		cloudflare({ viteEnvironment: { name: "ssr" } }),
+		tanstackStart(),
+		solid({ ssr: true }),
 	],
 });
