ffb95200349bd36185b13c711124d0df8ac76f05
Ibiyemi Abiodun <ibiyemi@intulon.com>
2025-11-29 01:21:08 -0500

    biome check

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,41 +1,40 @@
 import {
-  HeadContent,
-  Outlet,
-  Scripts,
-  createRootRouteWithContext,
-} from '@tanstack/solid-router'
-import { TanStackRouterDevtools } from '@tanstack/solid-router-devtools'
+	createRootRouteWithContext,
+	HeadContent,
+	Outlet,
+	Scripts,
+} from "@tanstack/solid-router";
+import { TanStackRouterDevtools } from "@tanstack/solid-router-devtools";
+import { Suspense } from "solid-js";
+import { HydrationScript } from "solid-js/web";
 
-import { HydrationScript } from 'solid-js/web'
-import { Suspense } from 'solid-js'
+import Header from "../components/Header";
 
-import Header from '../components/Header'
-
-import styleCss from '../styles.css?url'
+import styleCss from "../styles.css?url";
 
 export const Route = createRootRouteWithContext()({
-  head: () => ({
-    links: [{ rel: 'stylesheet', href: styleCss }],
-  }),
-  shellComponent: RootComponent,
-})
+	head: () => ({
+		links: [{ rel: "stylesheet", href: styleCss }],
+	}),
+	shellComponent: RootComponent,
+});
 
 function RootComponent() {
-  return (
-    <html>
-      <head>
-        <HydrationScript />
-      </head>
-      <body>
-        <HeadContent />
-        <Suspense>
-          <Header />
+	return (
+		<html>
+			<head>
+				<HydrationScript />
+			</head>
+			<body>
+				<HeadContent />
+				<Suspense>
+					<Header />
 
-          <Outlet />
-          <TanStackRouterDevtools />
-        </Suspense>
-        <Scripts />
-      </body>
-    </html>
-  )
+					<Outlet />
+					<TanStackRouterDevtools />
+				</Suspense>
+				<Scripts />
+			</body>
+		</html>
+	);
 }
